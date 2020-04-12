6729620bb1a15508a5ab48577412bb26321cc306
etcadinfinitum <lizzy.presland@gmail.com>
2020-04-12 12:06:13 -0700

    Add health ticker and star with collision
    next to do is spawning many stars...

---
+++ playground/Assets/GetConsumed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetConsumed : MonoBehaviour
+{
+    public HealthBarUpdate health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GameObject.FindGameObjectWithTag("Health").GetComponent<HealthBarUpdate>();
+    }
+
+    void OnCollisionEnter2D() {
+        if (health != null) {
+            float h = health.GetHealthBarValue();
+            health.SetHealthBarValue(h + 0.1f);
+        }
+        Destroy(gameObject, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ playground/Assets/HealthBarUpdate.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUpdate : MonoBehaviour
+{
+    private Image HealthBarImage;
+
+    public void SetHealthBarValue(float value)
+    {
+        HealthBarImage.fillAmount = value;
+        if(HealthBarImage.fillAmount < 0.2f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if(HealthBarImage.fillAmount < 0.4f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+    }
+
+    public float GetHealthBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+
+    public void SetHealthBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+
+    void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+        SetHealthBarValue(0.1f);
+    }
+}
